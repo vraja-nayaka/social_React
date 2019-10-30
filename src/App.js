6f9bdebeeb8ea869dada565c99b2fd5970ced3d3
vraja-nayaka <messiah.turner@gmail.com>
2019-10-30 20:54:48 +1000

    router 0.1

--- src/App.js
+++ src/App.js
@@ -4,36 +4,15 @@ import Navbar from './components/navbar/navbar';
 import Profile from './components/profile/profile';
 import Dialogs from './components/dialogs/dialogs';
 import index from './index.css';
+import { Route, BrowserRouter } from "react-router-dom";
 
-let DialogsData = [
-    {id: 1, name: "Леха"},
-    {id: 2, name: "Игорь"},
-    {id: 3, name: "Никита"},
-    {id: 4, name: "Хари"},
-    {id: 5, name: "Василий"}
-    ]
-let MessagesData = [
-    {id: 1, message: "Привет"},
-    {id: 2, message: "Хеллоу"},
-    {id: 3, message: "Как жизнь?"},
-    {id: 4, message: "Нормально!"},
-    {id: 5, message: "Ок...."}
-    ]
-
-let PostsData = [
-{id: 1, post: 'Всем здорова!', likesCount: 15},
-{id: 2, post: 'All right!', likesCount: 2},
-{id: 3, post: 'Happyness', likesCount: 74},
-{id: 4, post: 'Lets go!', likesCount: 4},
-{id: 5, post: 'Wow!', likesCount: 1}
-]
-
-const App = () => (<div className={index.grid}>
+const App = (props) => (<BrowserRouter>
+    <div className={index.grid}>
     <Header />
     <Navbar />
-    <Profile PostsData={ PostsData }/>
-    <Dialogs DialogsData={ DialogsData } MessagesData={MessagesData}/>
-    <a>helloWorld</a>
-</div>)
+    <Profile PostsData={props.PostsData}/>
+    <Dialogs DialogsData={props.DialogsData} MessagesData={props.MessagesData}/>
+        <a>helloWorld</a>
+    </div></BrowserRouter>)
 
 export default App 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let DialogsData = [
+    {id: 1, name: "Леха"},
+    {id: 2, name: "Игорь"},
+    {id: 3, name: "Никита"},
+    {id: 4, name: "Хари"},
+    {id: 5, name: "Василий"}
+    ]
+let MessagesData = [
+    {id: 1, message: "Привет"},
+    {id: 2, message: "Хеллоу"},
+    {id: 3, message: "Как жизнь?"},
+    {id: 4, message: "Нормально!"},
+    {id: 5, message: "Ок...."}
+    ]
+
+let PostsData = [
+{id: 1, post: 'Всем здорова!', likesCount: 15},
+{id: 2, post: 'All right!', likesCount: 2},
+{id: 3, post: 'Happyness', likesCount: 74},
+{id: 4, post: 'Lets go!', likesCount: 4},
+{id: 5, post: 'Wow!', likesCount: 1}
+]
+
+ReactDOM.render(<App PostsData={PostsData} DialogsData={DialogsData} MessagesData={MessagesData}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
