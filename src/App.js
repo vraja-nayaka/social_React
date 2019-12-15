d7043fcf18b4eba4296231c4959e5ce37ecd2af4
vraja-nayaka <messiah.turner@gmail.com>
2019-12-15 13:38:18 +1000

    react.lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import ProfileContainer from "./components/profile/ProfileContainer";
-import DialogsContainer from "./components/dialogs/DialogsContainer";
 import { Route, withRouter } from "react-router-dom";
 import NavbarConteiner from "./components/navbar/navbar";
-import UsersContainer from "./components/users/UsersContainer";
 import HeaderContainer from "./components/header/HeaderContainer";
 import LoginPage from "./components/login/Login";
 import { initializeApp } from "./redux/app-reducer";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import Preloader from './components/common/preloader/Preloader';
+import { withSuspense } from './hoc/withSuspense';
+const ProfileContainer = React.lazy(() => import('./components/profile/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/dialogs/DialogsContainer'));
+const UsersContainer = React.lazy(() => import('./components/users/UsersContainer'));
+
+
 
 class App extends React.Component {
   componentDidMount() {
@@ -17,23 +20,23 @@ class App extends React.Component {
   }
 
   render() {
- //   if (!this.props.initialized) {
- //     return <Preloader/>;
- //   } 
+    if (!this.props.initialized) {
+      return <Preloader />;
+    }
     return (
       <div className="grid">
         <HeaderContainer />
         <NavbarConteiner />
         <div className="content">
-          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-          <Route path="/dialogs" render={() => <DialogsContainer />} />
-          <Route path="/users" render={() => <UsersContainer />} />
+          <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)} />
+          <Route path="/dialogs" render={withSuspense(DialogsContainer)} />
+          <Route path="/users" render={withSuspense(UsersContainer)} />
           <Route path="/login" render={() => <LoginPage />} />
         </div>
       </div>
     );
-    }
   }
+}
 
 const mapStateToProps = state => ({
   initialized: state.app.initialized
