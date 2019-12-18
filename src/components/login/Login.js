d55ad35618768196f9076d8cd72f58f9cadc52d0
vraja-nayaka <52737608+vraja-nayaka@users.noreply.github.com>
2019-12-18 17:22:53 +1000

    Style Loginform with MUI

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react'
 
-import { Field, reduxForm } from "redux-form";
-import { Input, required } from "./../../utils/validators";
-import { login } from "./../../redux/auth-reducer";
-import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
-import style from "./../../utils/FormsControls.module.css";
+import { Field, reduxForm } from 'redux-form'
+import { Input, renderCheckbox, required } from './../../utils/validators'
+import { login } from './../../redux/auth-reducer'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+      }
+}))
 
 const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
   return (
@@ -16,7 +26,7 @@ const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
           type="text"
           label="Email address"
           component={Input}
-          placeholder={"Enter your email"}
+          placeholder={'Enter your email'}
           validate={[required]}
         />
       </div>
@@ -26,54 +36,61 @@ const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
           type="password"
           label="Password"
           component={Input}
-          placeholder={"Enter your pass"}
+          placeholder={'Enter your pass'}
           validate={[required]}
         />
       </div>
       <div>
-        <Field name="rememberMe" component={Input} type={"checkbox"} /> remember
-        me
+        <Field name="rememberMe" component={renderCheckbox} type={'сheckbox'} />{' '}
+        remember me
       </div>
       <div>
-        <button>Login</button>
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
       </div>
-      {error && (
-        <div className={style.formSummaryError}>{error}</div>
+      {captchaUrl && <img alt="captha" src={captchaUrl} />}
+      {captchaUrl && (
+        <Field
+          name="captcha"
+          label="You made too many attempts"
+          type="text"
+          component={Input}
+          placeholder={'Enter simbols from image'}
+          validate={[required]}
+        />
       )}
-      {captchaUrl && <img src={captchaUrl} />}
-      {captchaUrl && <Field
-        name="captcha"
-        label="You made too many attempts"
-        type="text"
-        component={Input}
-        placeholder={"Enter simbols from image"}
-        validate={[required]}
-      />
-      }
     </form>
-  );
-};
+  )
+}
 
-const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = props => {
+  const classes = useStyles()
+
   let onSubmit = data => {
-    props.login(data);
-  };
+    props.login(data)
+  }
   if (props.isAuth) {
-    return <Redirect to={"/profile"} />;
+    return <Redirect to={'/profile'} />
   }
   return (
-    <>
-      <h1>login</h1>
+    <Paper className={classes.root}>
+      <Typography variant="h5" noWrap>
+        SamuraiJS
+      </Typography>
       <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
-    </>
-  );
-};
+    </Paper>
+  )
+}
 
 let mapStateToProps = state => ({
   isAuth: state.auth.isAuth,
   captchaUrl: state.auth.captchaUrl
-});
+})
 
-export default connect(mapStateToProps, { login })(Login);;
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login)
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,48 +1,68 @@
 import React from 'react'
 import style from './FormsControls.module.css'
 
-export const required = value => (value || typeof value === 'number' ? undefined : 'Для отправки необходимо заполнить поле')
+import TextField from '@material-ui/core/TextField'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+
+export const required = value =>
+  value || typeof value === 'number'
+    ? undefined
+    : 'Для отправки необходимо заполнить поле'
 
 const maxLength = max => value =>
-    value && value.length > max ? `Максимальная длина - ${max} символов` : undefined
+  value && value.length > max
+    ? `Максимальная длина - ${max} символов`
+    : undefined
 export const maxLength30 = maxLength(30)
 
 export const Textarea = ({
-    input,
-    label,
-    type,
-    placeholder,
-    meta: { touched, error, warning }
+  input,
+  label,
+  type,
+  placeholder,
+  meta: { touched, error, warning }
 }) => (
-        <div>
-            <label>{label}</label>
-            <div className={`${style.formControl} ${touched && error && style.error}`}>
-                <textarea {...input} placeholder={placeholder} type={type} />
-                <div>
-                    {touched &&
-                        ((error && <span>{error}</span>) ||
-                            (warning && <span>{warning}</span>))}
-                </div>
-            </div>
-        </div>
-    )
+  <div>
+    <label>{label}</label>
+    <div className={`${style.formControl} ${touched && error && style.error}`}>
+      <textarea {...input} placeholder={"placeholder"} type={type} />
+      <div>
+        {touched &&
+          ((error && <span>{error}</span>) ||
+            (warning && <span>{warning}</span>))}
+      </div>
+    </div>
+  </div>
+)
 
-    export const Input = ({
-        input,
-        label,
-        type,
-        placeholder,
-        meta: { touched, error, warning }
-    }) => (
-            <div>
-                <label>{label}</label>
-                <div className={`${style.formControl} ${touched && error && style.error}`}>
-                    <input {...input} placeholder={placeholder} type={type} />
-                    <div>
-                        {touched &&
-                            ((error && <span>{error}</span>) ||
-                                (warning && <span>{warning}</span>))}
-                    </div>
-                </div>
-            </div>
-        )
+export const Input = ({
+  input,
+  label,
+  type,
+  placeholder,
+  meta: { touched, invalid, error },
+  ...custom
+}) => (
+  <TextField
+    label={label}
+    error={touched && invalid}
+    helperText={touched && error}
+    {...input}
+    {...custom}
+  />
+)
+
+export const renderCheckbox = ({ input, label }) => (
+  <div>
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={input.value ? true : false}
+          onChange={input.onChange}
+        />
+      }
+      label={label}
+    />
+  </div>
+)
