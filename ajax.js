7824eb549c01c5c31737491c420a308374e4c433
vraja-nayaka <messiah.turner@gmail.com>
2020-02-03 12:56:15 +1000

    dialogs some fix

--- ajax.js
+++ ajax.js
@@ -1,16 +1,17 @@
 function makeRequest() {
-    const resultBlock = document.querySelector("#result");
   const pageNumberEl = document.querySelector("#page-number");
-  const clickMeButton = documtnt.querySelector("#click-me");
   clickMeBotton.addEventListener("click", makeRequest);
-    $.ajax(`https://repetitora.net/api/JS/Imagas?page=${pageNumberEl.value}&count=1`, {
-          success: function (data) {
-            data.forEack(el => {
-      const img= document.cteateElement('img');
-      img.src = el.thumbnail;
-      document.querySelector('#result').appendCild(img);
-    });
-          }
-    });
-  }
-  Export default MakeRequest
+  $.ajax(
+    `https://repetitora.net/api/JS/Imagas?page=${pageNumberEl.value}&count=1`,
+    {
+      success: function(data) {
+        data.forEack(el => {
+          const img = document.cteateElement("img");
+          img.src = el.thumbnail;
+          document.querySelector("#result").appendCild(img);
+        });
+      }
+    }
+  );
+}
+export default makeRequest;
