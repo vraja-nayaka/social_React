2e2e2d1bc5da395bd14f58571fd701513d313f5f
vraja-nayaka <messiah.turner@gmail.com>
2020-02-15 14:43:40 +1000

    starting typescript

---
+++ src/redux/app-reducer.tsx
@@ -0,0 +1,41 @@
+import { getAuthUserData } from "./auth-reducer";
+
+const INITIALIZED_SUCCESS = "INITIALIZED_SUCCESS";
+const TOGGLE_THEME = "TOGGLE_THEME";
+
+let initialState = {
+  initialized: false,
+  isDarkTheme: false
+};
+
+const appReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case INITIALIZED_SUCCESS:
+      return {
+        ...state,
+        initialized: true
+      };
+    case TOGGLE_THEME:
+      return {
+        ...state,
+        isDarkTheme: !state.isDarkTheme
+      };
+    default:
+      return state;
+  }
+};
+
+
+export const toggleTheme = () => ({ type: TOGGLE_THEME });
+export const initializedSuccess = () => ({ type: INITIALIZED_SUCCESS });
+
+export const initializeApp = () => (dispatch: any) => {
+  let promise = dispatch(getAuthUserData());
+  //dispatch(somethingelse());
+  //dispatch(somethingelse());
+  Promise.all([promise]).then(() => {
+    dispatch(initializedSuccess());
+  });
+};
+
+export default appReducer;
